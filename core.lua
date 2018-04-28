f36296598954865afcfbe0ec9a6b20541ba2141f
Banksy <tiaguinhu@gmail.com>
2018-04-28 11:33:07 +0200

    corrected frame placement

--- core.lua
+++ core.lua
@@ -2,10 +2,7 @@ local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_ENTERING_WORLD");
 frame:SetScript("OnEvent", function(self, event, ...)
     if (event == 'PLAYER_ENTERING_WORLD') then
-        local p = _G["PartyMemberFrame"..i];
-        p:SetUserPlaced(true);
-        p:SetMovable(false);
-        p:ClearAllPoints();
-        p:SetPoint("TOPLEFT", UIParent, "TOPLEFT", -200, -200);
+        PartyMemberFrame1:ClearAllPoints()
+        PartyMemberFrame1:SetPoint("LEFT", 200, 200)
     end
-end
+end)
